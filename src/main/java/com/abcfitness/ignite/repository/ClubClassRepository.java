dec013d3ba96070882502e52bd9ba056d70a0a4a
deadhead <deadhead00@pm.me>
2025-01-21 20:52:02 +0530

    handled engulfing condition

--- src/main/java/com/abcfitness/ignite/repository/ClubClassRepository.java
+++ src/main/java/com/abcfitness/ignite/repository/ClubClassRepository.java
@@ -6,16 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.abcfitness.ignite.entity.ClubClass;
 
 @Repository
 public interface ClubClassRepository extends JpaRepository<ClubClass, Long> {
 
-    @Query("select count(cc)>0 from ClubClass cc join Club c on cc.club.id=c.id  where c.id=:clubId and (( :startDate>=cc.startDate and :startDate<=cc.endDate ) or ( :endDate>=cc.startDate and :endDate<=cc.endDate))")
+    @Query("select count(cc)>0 from ClubClass cc join Club c on cc.club.id=c.id  where c.id=:clubId and (( :startDate>=cc.startDate and :startDate<=cc.endDate ) or ( :endDate>=cc.startDate and :endDate<=cc.endDate) or ( :startDate<=cc.startDate and :endDate>=cc.endDate ))")
     public Boolean classesExistsInGivenDates(@Param("startDate") Date startDate, @Param("endDate") Date endDate,
             @Param("clubId") Long clubId);
-
-    
 
 }
